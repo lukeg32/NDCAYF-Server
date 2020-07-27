923f264e25f8dc39ffd2e1d87d472e042e2aadef
Lukeg32 <lgantar19@georgefox.edu>
2020-07-27 11:14:10 -0700

    added some constants

--- util/networking/networkConfig.hpp
+++ util/networking/networkConfig.hpp
@@ -8,6 +8,17 @@
 #define PORT  12345
 #define SUPERSECRETKEY_SERVER "ndcayfserver"
 #define SUPERSECRETKEY_CLIENT "ndcayfclient"
+
+// protocols
+#define PING "ping"
+#define PONG "pong"
+
+// helpers
+#define SENDPONG 1
+#define SENDPING 2
+#define ISSTATE 3
+#define SPEED 4
+
 #define MAXSERVERS 5
 
 // not sure if this value is important enough
