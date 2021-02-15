3ddf0f4db3094eb3869183e7eeedb48392dbbc9b
lukeg32 <lukegantar@gmail.com>
2021-02-14 18:37:08 -0800

    stuff

--- util/networking/serverOOP.cpp
+++ util/networking/serverOOP.cpp
@@ -503,7 +503,7 @@ bool TCP::musicGet()
 bool TCP::musicMain()
 {
     ifstream myfile;
-    string thing("Fine.wav");
+    string thing("Start.wav");
     struct musicHeader header;
     char* theData = load_wav(thing, header.channels, header.sampleRate, header.bitsPerSample, header.dataSize, header.format);
     printf("channel: %d, sampleRate: %d, bps %d, size: %d\n", header.channels,
